7dde77d4290ac539a5cc5790ad23641a702636f1
ican <dawwgadican@gmail.com>
2026-08-01 09:46:48 +0700

    nambah efek di js

--- ican.js
+++ ican.js
@@ -1,1 +1,5 @@
 console.log
+const tombolinstagram =document.getelementbyid("btn-instagram");
+tombolIg.addEventListener('click', function() {
+    alert("Terima kasih sudah berkunjung! Kamu akan diarahkan ke profil Instagram saya 😉");
+});
