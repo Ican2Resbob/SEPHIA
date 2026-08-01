3c169de78acd802fc182c3fa1d93e953a9bc87b4
ican <dawwgadican@gmail.com>
2026-08-01 11:06:00 +0700

    dark mode

--- ican.js
+++ ican.js
@@ -3,3 +3,14 @@ const tombolIg = document.getElementById("btn-instagram");
 tombolIg.addEventListener('click', function() {
     alert("Terima kasih sudah berkunjung! Kamu akan diarahkan ke profil Instagram saya 😉");
 });
+const btnTheme = document.getElementById('btn-theme');
+
+btnTheme.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        btnTheme.textContent = '☀️ Mode Terang';
+    } else {
+        btnTheme.textContent = '🌙 Mode Gelap';
+    }
+});
